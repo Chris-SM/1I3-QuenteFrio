034f7b7080b37619503a51278986fa1dbd0adfe4
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-17 21:50:45 -0300

    Finalizada

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿
+using System.Security.Cryptography;
+Console.Clear();
+int RoboEscolho = RandomNumberGenerator.GetInt32(1,101);
+int UsuarioEscolheu;
+bool Acertou = false;
+
+Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.BackgroundColor = ConsoleColor.Gray;
+Console.Write("----- Estou pensando num numero -----");
+Console.ResetColor();
+Console.Write("\n\n\n");
+while(!Acertou)
+{
+    UsuarioEscolheu = Convert.ToInt32(Console.ReadLine());
+    if(UsuarioEscolheu == RoboEscolho)
+    {
+        Acertou = true;
+    }
+    else if(UsuarioEscolheu < RoboEscolho+3 && UsuarioEscolheu > RoboEscolho-3)
+    {
+        Console.WriteLine("Está pelando");
+    }
+    else if(UsuarioEscolheu < RoboEscolho+10 && UsuarioEscolheu > RoboEscolho-10 )
+    {
+        Console.WriteLine("Está quente");
+    }
+    else if(UsuarioEscolheu < RoboEscolho+30 && UsuarioEscolheu > RoboEscolho-30 )
+    {
+        Console.WriteLine("Está congelando");
+    }
+    else
+    {
+        Console.WriteLine("Está frio");
+    }
+    Console.Write("");
+}
+Console.BackgroundColor = ConsoleColor.White;
+Console.ForegroundColor = ConsoleColor.DarkCyan;
+Console.Write("----- Você ganhou. Parabéns -----");
+Console.ResetColor();
